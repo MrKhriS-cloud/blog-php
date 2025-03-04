6fe3ed324d0b3295cec4266d793af5a1d5ce8749
Christian Soh <christian.soh@etu.univ-nantes.fr>
2025-03-04 16:06:40 +0100

    Correction suppression PHPMailer et ajout de .gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e9044b39ce6dd9c5d2c6cdc44b582d823e8a7e28',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e9044b39ce6dd9c5d2c6cdc44b582d823e8a7e28',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
